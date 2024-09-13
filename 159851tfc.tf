0fa7af3eeb594c189da0c08fb7e742505e279975
Abhinav Tripathi <abhi.tripathi14@gmail.com>
2024-09-13 13:28:02 +0530

    Update 159851tfc.tf

--- 159851tfc.tf
+++ 159851tfc.tf
@@ -27,7 +27,7 @@ data "tfe_project" "colibri" {
 
 resource "tfe_workspace" "ws_workspace" {
 
-  name         = "Test-2" # each.value.ws_name
+  name         = "Test-3" # each.value.ws_name
   organization = local.tfc_org_name
   description  = "Test-2" 
   tag_names = ["databricks-workspace"]

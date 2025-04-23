5a1d8a7e77574ec021e821974769a5f6383c2897
Abhinav Tripathi <abhi.tripathi14@gmail.com>
2025-04-23 12:17:36 +0530

    Update tfc.tf

--- tfc.tf
+++ tfc.tf
@@ -20,7 +20,7 @@ output "tfe_oauth_client_vcs_client_oauth_token_id" {
 
 data "tfe_project" "projname" {
 
-  name         = "Default Project"
+  name         = "New-project-5"
   organization = local.tfc_org_name
 }
 
